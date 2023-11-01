22d29ff247affcd5f2a80b04100a79fa73c51b52
César <cesar.penuela@hotmail.com>
2023-10-31 21:11:10 -0500

    first advance in javascript

--- js/js.mjs
+++ js/js.mjs
@@ -1,27 +1,44 @@
-function E(id) {
-  return document.getElementById(id);
-}
 
-const input = E("input");
-const addPlayer = E("addPlayer");
-const listGroup = E("list-group");
-const list = E("list");
+document.addEventListener("DOMContentLoaded", function () {
+  const inputElement = document.getElementById("input");
+  const listElement = document.getElementById("list-group");
+  const buttonPlay = document.getElementById("button-play");
+  
 
-let players = ["julio", "lopera", "yeni"];
+  const names = [];
+  
 
-addPlayer.addEventListener("click", () => {
-  let player = input.value;
-  input.value = "";
-  input.focus();
-  if (player) {
-    players.push(player);
-    list.style.display = "block";
-    
+  function updateList() {
+      listElement.innerHTML = ""; 
+      console.log(names)
+      names.forEach((name, index) => {
+          const listItem = document.createElement("li");
+          listItem.textContent = name;
+          console.log(listItem)
+          
+          
+          
+          listElement.appendChild(listItem);
+      });
   }
+  
+
+  document.getElementById("addPlayer").addEventListener("click", function () {
+      const playerName = inputElement.value;
+      if (playerName.trim() !== "") {
+          console.log("nombre agregado",playerName)
+          names.push(playerName);
+          inputElement.value = ""; 
+          updateList();
+      }
+  });
+  
+  
+  buttonPlay.addEventListener("click", function () {
+      
+      console.log("JUGAR button clicked");
+      names.shift()
+      updateList()
+          
+  });
 });
-
-function showPlayers() {
-  for (let index = 0; index < players.length; index++) {
-    console.log(players[index]);
-  }
-}
